84488e06eef2ce37989d594a11d2569e4eb21f89
Sailor Ong <valleywind15@gmail.com>
2021-10-26 18:25:34 -0600

    set up

---
+++ cmake-build-debug/CMakeFiles/project_2.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/roliasong/Desktop/cs236/Interpreter/AddAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/AddAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/ColonAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/ColonAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/ColonDashAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/ColonDashAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/CommaAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/CommaAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/CommentAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/CommentAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/DatalogProgram.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/DatalogProgram.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/EOFAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/EOFAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/FactsAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/FactsAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/ID.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/ID.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/LeftParenAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/LeftParenAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/Lexer.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/Lexer.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/MultiComment.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/MultiComment.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/MultiplyAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/MultiplyAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/Parameter.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/Parameter.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/PeriodAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/PeriodAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/Predicate.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/Predicate.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/QmarkAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/QmarkAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/QueriesAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/QueriesAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/RightParenAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/RightParenAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/Rule.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/Rule.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/RulesAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/RulesAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/SchemesAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/SchemesAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/StringAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/StringAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/Token.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/Token.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/UndefinedAutomaton.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/UndefinedAutomaton.cpp.o"
+  "/Users/roliasong/Desktop/cs236/Interpreter/main.cpp" "/Users/roliasong/Desktop/cs236/Interpreter/cmake-build-debug/CMakeFiles/project_2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
